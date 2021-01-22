b5f1add55d6a9adde8fd3ef17d58b7d6935d7851
Alex Sychevskiy <teran0404@mail.ru>
2021-01-22 15:26:37 +0300

    script for syslog

--- script1.sh
+++ script1.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+#show log dir
+ls -la /var/log
+echo $stdout
+#checking file availability
+check=/var/log/syslog
+if test -f $check;
+  then
+	echo "################### last 10 lines"
+	tail $check
+	echo "################### first 10 lines"
+	head $check
+	echo "################### first 10 lines today"
+	cat $check | grep -n 'Jan 22' | head
+	echo "################### last 10 lines of crontab"
+	cat $check | grep crontab | tail
+  else
+  	echo "File not found"
+fi
