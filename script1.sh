289c456226c3b25ee7eb7c6cde023bcf1b161f12
Alex Sychevskiy <teran0404@mail.ru>
2021-01-24 22:52:19 +0300

    fix script

--- script1.sh
+++ script1.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-###=
-time=`date +"%b %d"`
+###
+#Variables
+FORMATTED_DATE=`date +"%b %d"`
+LOG_FILE=/var/log/syslog
+LOG_DIR=~/for_syslog
+#Check and create target directory
+if ! [ -d $LOG_DIR ];
+  then
+  	mkdir $LOG_DIR
+  	echo "target directory created"
+fi
 #show log dir
 ls -la /var/log > ~/for_syslog/show_dir_log.log
-#checking file availability
-check=/var/log/syslog
-if [ -f $check ];
+#Check syslog and redirecting output to a files
+if [ -f $LOG_FILE ];
   then
 	echo "starting file operations"
-	tail $check > ~/for_syslog/last_10_lines_in_syslog.log
-	head $check > ~/for_syslog/first_10_line_in_syslog.log
-	head $check | grep -n "$time" > ~/for_syslog/first_10_lines_today_in_syslog.log
-	tail $check | grep crontab > ~/for_syslog/last_10_lines_of_crontab_in_syslog.log
+	tail $LOG_FILE > ~/for_syslog/last_10_lines_in_syslog.log
+	head $LOG_FILE > ~/for_syslog/first_10_line_in_syslog.log
+	head $LOG_FILE | grep -n "$time" > ~/for_syslog/first_10_lines_today_in_syslog.log
+	tail $LOG_FILE | grep crontab > ~/for_syslog/last_10_lines_of_crontab_in_syslog.log
 	echo "done"
   else
   	echo "File not found"
 fi
 ###
+#deleting files older than 10 minutes
 find ~/for_syslog -mmin +10 -type f -exec rm -fv {} \;
