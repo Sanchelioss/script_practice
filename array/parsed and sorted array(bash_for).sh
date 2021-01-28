547be0fa3caab263b0be9a8a17a15db80aaff55c
Alex <teran0404@mail.ru>
2021-01-28 14:44:03 +0300

    parsed and sorted array(bash_for)

---
+++ array/parsed and sorted array(bash_for).sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+INPUT_ARRAY=("$@")
+
+###sorting by variable value
+get_parsed_array () {
+	VALUE_LIMIT=7
+	for i in ${INPUT_ARRAY[@]}; do
+		if [[ $i -ge $VALUE_LIMIT ]]; then
+			OUTPUT_ARRAY+=($i)
+	    fi
+	done
+}
+get_parsed_array ${INPUT_ARRAY[@]}
+
+###sort ascending
+get_sort_array () {
+OUTPUT_ARRAY_LEN="${#OUTPUT_ARRAY[@]}"
+for (( i=0; i < OUTPUT_ARRAY_LEN; i++ ))
+do
+    for (( j = OUTPUT_ARRAY_LEN-1; j > i; j-- ))
+    do
+        if (( ${OUTPUT_ARRAY[$j-1]} > ${OUTPUT_ARRAY[$j]} )); then
+            temp=${OUTPUT_ARRAY[$j-1]}
+            OUTPUT_ARRAY[$j-1]=${OUTPUT_ARRAY[$j]}
+            OUTPUT_ARRAY[$j]=$temp
+        fi
+    done
+done
+
+#for (( l=0; l<OUTPUT_ARRAY_LEN; l++))
+#do
+#    echo -e "${OUTPUT_ARRAY[$l]}"
+#done
+}
+get_sort_array ${OUTPUT_ARRAY[@]}
+echo "${OUTPUT_ARRAY[@]}"
