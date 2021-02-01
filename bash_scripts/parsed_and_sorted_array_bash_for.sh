448121ecda54d3b5bafd0c1f87427a3252cd6f67
Alex <teran0404@mail.ru>
2021-02-01 11:43:23 +0300

    fixed scripts

---
+++ bash_scripts/parsed_and_sorted_array_bash_for.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+INPUT_ARRAY=("$@")
+
+
+get_parsed_array () {
+	VALUE_LIMIT=7
+	for i in ${INPUT_ARRAY[@]}; do
+		if [[ $i -ge $VALUE_LIMIT ]]
+        then
+			OUTPUT_ARRAY+=($i)
+	    fi
+	done
+}
+
+
+get_parsed_array ${INPUT_ARRAY[@]}
+
+
+get_sorted_array () {
+OUTPUT_ARRAY_LEN="${#OUTPUT_ARRAY[@]}"
+    for (( i=0; i < OUTPUT_ARRAY_LEN; i++ ))
+    do
+        for (( j = OUTPUT_ARRAY_LEN-1; j > i; j-- ))
+        do
+            if (( ${OUTPUT_ARRAY[$j-1]} > ${OUTPUT_ARRAY[$j]} ))
+            then
+                temp=${OUTPUT_ARRAY[$j-1]}
+                OUTPUT_ARRAY[$j-1]=${OUTPUT_ARRAY[$j]}
+                OUTPUT_ARRAY[$j]=$temp
+            fi
+        done
+    done
+}
+
+get_sorted_array ${OUTPUT_ARRAY[@]}
+
+echo "${OUTPUT_ARRAY[@]}"
---
+++ bash_scripts/parsed_and_sorted_array_bash_while.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+INPUT_ARRAY=("$@")
+INPUT_ARRAY_LEN="${#INPUT_ARRAY[@]}"
+
+
+get_parsed_array () {
+	VALUE_LIMIT=7
+    i=0
+	while [[ $i -lt ${INPUT_ARRAY_LEN} ]]; do
+		if [[ ${INPUT_ARRAY[$i]} -gt $VALUE_LIMIT ]]
+        then
+			OUTPUT_ARRAY+=(${INPUT_ARRAY[$i]})
+	    fi
+        (( i++ ))
+	done
+}
+
+
+get_parsed_array ${INPUT_ARRAY_LEN[@]}
+
+
+get_sorted_array () {
+OUTPUT_ARRAY_LEN="${#OUTPUT_ARRAY[@]}"
+for (( i=0; i < OUTPUT_ARRAY_LEN; i++ ))
+    do
+        for (( j = OUTPUT_ARRAY_LEN-1; j > i; j-- ))
+        do
+            if (( ${OUTPUT_ARRAY[$j-1]} > ${OUTPUT_ARRAY[$j]} ))
+            then
+                temp=${OUTPUT_ARRAY[$j-1]}
+                OUTPUT_ARRAY[$j-1]=${OUTPUT_ARRAY[$j]}
+                OUTPUT_ARRAY[$j]=$temp
+            fi
+        done
+    done
+}
+
+
+get_sorted_array ${OUTPUT_ARRAY[@]}
+echo "${OUTPUT_ARRAY[@]}"
