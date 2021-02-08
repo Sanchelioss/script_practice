bc65b6e4a69cfc71c365c49f9d5a5d9e0fb95fb6
Alex <teran0404@mail.ru>
2021-02-08 13:47:26 +0300

    re-fix API script

--- scripts/python3_scripts/API_training.py
+++ scripts/python3_scripts/API_training.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
 
+import requests
 
-def get_weather_api_response():
+def get_weather_api_response(api_responce):
 	import json
-	import requests
-	try:
-		responce_json = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Hrodna&appid=b6627e1c2f78708e4c3b1abed614bfcd')
-		if responce_json:
-			json_format = responce_json.json()
-			return json_format
-	except:
-		print("Link is invalid")
+	if api_responce:
+		responce_json = api_responce.json()
+		return responce_json
 
 
-def get_city_weather(json_format):
-	city = str("city: " + json_format['name'])
-	temperature_kelvin = int(json_format['main']['temp'])
+def get_city_weather(responce_json):
+	city = str("city: " + responce_json['name'])
+	temperature_kelvin = int(responce_json['main']['temp'])
 	temperature_celsiy = ("temperature: " + str(temperature_kelvin - 273) + "C")
-	weather = ("weather: " + json_format['weather'][0]['main'])
-	humidity = ("humidity: " + str(json_format['main']['humidity']) + "%")
+	weather = ("weather: " + responce_json['weather'][0]['main'])
+	humidity = ("humidity: " + str(responce_json['main']['humidity']) + "%")
+
 	return city, temperature_celsiy, weather, humidity;
 
-json_format = get_weather_api_response()
-if json_format is None:
+
+responce_json = None
+
+
+try:
+	api_responce = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Hrodna&appid=b6627e1c2f78708e4c3b1abed614bfcd')
+	responce_json = get_weather_api_response(api_responce)
+except:
+	print("Link is invalid")
+
+
+
+if responce_json is None:
 	print("Request is BAD. Aborting...")
 else:
-	result = get_city_weather(json_format)
-	print(f'{result[0]}', '\n' f'{result[1]}', '\n' f'{result[2]}', '\n' f'{result[3]}')
+	result = get_city_weather(responce_json)
+	print(f'{result[0]}\n{result[1]}\n{result[2]}\n{result[3]}')
